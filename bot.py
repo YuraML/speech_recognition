ebd3b35d116dcdfcaea9a8bd3163af3f8d409487
YuraML <malashtayuriy@gmail.com>
2023-06-15 18:51:20 +0400

    setup DialogFlow

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-
+from google.cloud import dialogflow
 
 load_dotenv()
 logging.basicConfig(
@@ -14,10 +14,23 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def start(update: Update, context: CallbackContext) -> None:
+def create_session(project_id, session_id):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    return session, session_client
+
+
+def get_response(session, session_client, text, language_code):
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(request={"session": session, "query_input": query_input})
+    return response.query_result.fulfillment_text
+
+
+def start_command(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
     update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!',
+        fr'Привет {user.mention_markdown_v2()}\!',
         reply_markup=ForceReply(selective=True),
     )
 
@@ -26,8 +39,19 @@ def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Help!')
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+def reply_to_user(update: Update, context: CallbackContext) -> None:
+    project_id = os.getenv('PROJECT_ID')
+    session_id = os.getenv('SESSION_ID')
+    text = update.message.text
+    language_code = 'ru'
+
+    session, session_client = create_session(project_id, session_id)
+    response = get_response(session, session_client, text, language_code)
+
+    if not response:
+        response = "Не совсем понимаю, о чем вы. Можете уточнить?"
+
+    update.message.reply_text(response)
 
 
 def main() -> None:
@@ -36,10 +60,10 @@ def main() -> None:
 
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("start", start_command))
     dispatcher.add_handler(CommandHandler("help", help_command))
 
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, reply_to_user))
 
     updater.start_polling()
 
